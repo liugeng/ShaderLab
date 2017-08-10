cdf95f984bade9a04da859ec76b009a8c73ccaf1
liugeng <smilesnow0@126.com>
2017-08-10 18:41:45 +0800

    动态模糊

---
+++ 动态模糊/MotionBlurEffect.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MotionBlurEffect : MonoBehaviour {
+
+	#region 属性
+	
+	//实现动态模糊的着色器
+	[SerializeField]
+	private Shader _motionBlurShader;
+
+	//使用_motionBlurShader的材质
+	private Material _motionBlurMat;
+	private Material motionBlurMat { get { if (!_motionBlurMat && _motionBlurShader) { _motionBlurMat = new Material(_motionBlurShader); } return _motionBlurMat; } }
+
+	//动态模糊的中心点
+	public Vector2 _center = new Vector2(0.5f, 0.5f);
+
+	//模糊强度
+	[Range(-0.5f, 0.5f)]
+	public float _intensity = 0.125f;
+
+	//迭代次数
+	[Range(5, 50)]
+	public int _iterateCount = 16;
+
+	#endregion
+
+
+	void Awake () {
+		//判断是否支持屏幕特效
+		if (!SystemInfo.supportsImageEffects) {
+			enabled = false;
+			return;
+		}
+
+		if (!_motionBlurShader) {
+			_motionBlurShader = Shader.Find("Custom/MotionBlurEffect");
+		}
+	}
+
+	//此函数在完成所有图片渲染后调用，用来渲染图片后期效果
+	private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+		if (motionBlurMat) {
+			motionBlurMat.SetVector("_Center", _center);
+			motionBlurMat.SetFloat("_Intensity", _intensity * 0.085f);
+			motionBlurMat.SetInt("_IterateCount", _iterateCount);
+
+			//---------------------------------------[ Blit ]-------------------------------------------
+			// 实现从源纹理到目标纹理的拷贝过程
+			// public static void Blit(Texture source,RenderTexture dest);  
+			// public static void Blit(Texture source, RenderTexture dest, Material mat, int pass = -1);
+			// public static void Blit(Texture source, Material mat, int pass = -1);
+			//
+			// 参数 source: 原始纹理
+			// 参数 dest  : 目标渲染纹理，如果是null，表示直接将原始纹理拷贝到屏幕上
+			// 参数 mat   : 材质，通过材质的shader实现后期效果
+			// 参数 pass  : 指定使用的pass，默认值-1，表示使用所有的pass
+			//------------------------------------------------------------------------------------------
+			Graphics.Blit(source, motionBlurMat);
+
+		} else {
+			Graphics.Blit(source, destination);
+		}
+	}
+}
---
+++ 动态模糊/MotionBlurEffect.shader
@@ -0,0 +1,74 @@
+﻿Shader "Custom/MotionBlurEffect"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			sampler2D _MainTex;
+			float2 _Center;
+			float _Intensity;
+			int _IterateCount;
+
+			
+			// 算出纹理坐标离动态模糊中心的差值
+			// 在循环里，每次对这个差值做一点缩放
+			// 然后用这个缩放后的uv坐标去采样得到一个颜色值
+			// 将多次采样得到的颜色相加再求出一个平均值
+			// 就得到最终的颜色了
+			
+			// 现在用的是x、y轴缩放相同的值
+			// 如果只在x方向上对uv坐标缩放
+			// 就能得到沿x方向的径向模糊效果了
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				float2 uv = i.uv - _Center;
+				float scale = 1;
+				float4 color = 0;
+				
+				for (int j = 0; j < _IterateCount; j++) {
+					color += tex2D(_MainTex, uv * scale + _Center);
+					scale = 1 + j * _Intensity;
+				}
+				
+				color /= _IterateCount;
+				
+				return color;
+			}
+			ENDCG
+		}
+	}
+}
